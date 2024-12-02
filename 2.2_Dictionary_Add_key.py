1fd903cff3f3ed987567928033686d8c82ba19c6
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-02 16:00:36 +0530

    [Aman] Add: Python script to add a key to a dictionary.

---
+++ 2.2_Dictionary_Add_key.py
@@ -0,0 +1,17 @@
+#declaring a dictionary student marks with key Name and value marks
+student_marks={
+    "Rahul":34,
+    "akash":80
+}
+
+#Printing the orignal dictionary  
+print("Original Dictionary:",student_marks)
+
+#adding a new key 
+student_marks["Nakul"]=40
+
+#printing the Dictionary after adding a new key 
+print("After Adding a new key to dictionary",student_marks)
+
+
+
