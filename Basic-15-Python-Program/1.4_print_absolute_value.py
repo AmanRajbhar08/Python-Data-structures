83b6ba08bf83644f0adad00302157966916e87ce
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-03 14:52:23 +0530

    [Aman] Add: program to Print the document or docstring of in build functions in python

---
+++ Basic-15-Python-Program/1.4_print_absolute_value.py
@@ -0,0 +1,6 @@
+#abs() is one the in build function which returns the abosulte value of a number
+argument1="abs()"
+argument2="len()"
+#printing the docstring of the argument
+print(f"Docstring of {argument1}  {abs.__doc__}")
+print(f"Docstring of the {argument2}  {len.__doc__}")
