fcb2cbea3a7ce03bdc68f79dbc0386ace330ac52
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-03 13:51:01 +0530

    [Aman] Add:Program to display first and the last element from the list

---
+++ Basic-15-Python-Program/1.2_Display_item_from_list.py
@@ -0,0 +1,5 @@
+# color list
+color_list = ["Red","Green","White" ,"Black"]
+
+#display fisrt and last colour from the list 
+print(f"First color from list is {color_list[0]} and Last color from list is {color_list[-1]}")
