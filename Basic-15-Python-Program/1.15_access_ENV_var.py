1deee7b6ed14701af93d99a210055e23df74a0fd
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-03 16:56:43 +0530

    [Aman] Add: python program to access environment variables.

---
+++ Basic-15-Python-Program/1.15_access_ENV_var.py
@@ -0,0 +1,11 @@
+#import os module 
+import os
+
+
+#printing the environment variables of this computer in kye values pair
+for key,value in os.environ.items():
+    print(f"{key} : {value}")
+   
+    
+    
+
