e6e271974817a00c9a5a8ee312db47af7667fa05
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-03 14:59:46 +0530

    [Aman] Add: Python program to print the calendar of a given month and year.

---
+++ Basic-15-Python-Program/1.5_Print_calender.py
@@ -0,0 +1,8 @@
+#importing the calender module
+import calendar
+
+year=int(input("Enter the Year :"))
+month=int(input("Enter the month:"))
+
+#printing the month accoding to the year usinf valender module 
+print(calendar.month(year,month))
