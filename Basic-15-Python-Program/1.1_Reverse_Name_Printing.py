26c830856a65033907d33ee9de64954dae832114
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-03 13:23:52 +0530

    [Aman] Add:Python Program to print user name in reverse order

---
+++ Basic-15-Python-Program/1.1_Reverse_Name_Printing.py
@@ -0,0 +1,7 @@
+#input first name 
+first_name=input("Enter your First Name:")
+#input last name
+Last_name=input("Enter your Last Name :")
+
+#print Name is the reverse order
+print(f"Your Name is Reverse Order : {Last_name} {first_name}")
