3af167f66742105933138fbd58d3cdab31f5befc
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 01:03:14 +0530

    [Aman] Add:Python program to convert a list to a tuple

---
+++ Tuple/1.7_list_to_tuple.py
@@ -0,0 +1,13 @@
+#sample list
+sample_list=[1,2,3,4,5]
+
+#converting list to tuple 
+sample_tuple=tuple(sample_list)
+
+#printing the list
+print(f"List :{sample_list}")
+#printing the list in tuple
+print(f"After converting list to tuple {sample_tuple}")
+
+
+
