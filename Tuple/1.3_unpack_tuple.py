5e90b638f95ef3ad65b88929f21f41b5670277c2
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 00:31:02 +0530

    [Aman] Add:Python program to unpack a tuple in several variables

---
+++ Tuple/1.3_unpack_tuple.py
@@ -0,0 +1,9 @@
+#tuple cration with diff data types 
+mix_data_type_tuple=(1,"Aman",[1,2],True)
+
+#unpacking tuple into various variable 
+num, name,lis,boolean=mix_data_type_tuple
+print(f"{num}")
+print(f"{name}")
+print(f"{lis}")
+print(f"{boolean}")
