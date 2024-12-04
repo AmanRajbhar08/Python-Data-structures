138bf73b55521c427393e5cb9da9109831d00243
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-04 12:35:35 +0530

    [Aman] Add: Program to count the number of substirng in a stirng

---
+++ String/1.10_substr_in_str_count.py
@@ -0,0 +1,22 @@
+def count_substr(string,substirng):
+    """
+    def: function to count the num of time a sub string occured in a stirng 
+
+    Args:
+        string (stirng)
+        substirng (stirng)
+
+    Returns:
+       count of substirng 
+    """
+    count=string.count(substirng) 
+    return count
+
+#input a sample stirng 
+sample_str=input("Enter a string word or stentance :")
+
+#input the substirgn for count 
+sub_string=input("Enter the sub string you want to count in string :")
+
+#printing the no of time substirng occurent by calling count substring method 
+print(f"{sub_string} occured {count_substr(sample_str,sub_string)} times in the string {sample_str}")
