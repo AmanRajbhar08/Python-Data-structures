30725c96c82c16fee28dac05ea59f743ab6fe124
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-04 15:05:57 +0530

    [Aman] Add:Python program to clone or copy a list.

---
+++ List/1.7_clone_list.py
@@ -0,0 +1,9 @@
+#original list 
+sample_list=[1,34,3,45,4,39,90]
+
+#cloneing or copying the original list to clone list
+clone_list=list(sample_list)
+#printing orignal list 
+print(f"Original List :{sample_list}")
+#printing cloned List 
+print(f"cloned List    :{clone_list}")
