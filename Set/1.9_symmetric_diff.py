cee6a7c677343d91ec80cba553c551a4b2311668
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 13:05:37 +0530

    [Aman] Add: Python program to create a symmetric difference.

---
+++ Set/1.9_symmetric_diff.py
@@ -0,0 +1,13 @@
+#sample sets
+sample_set1 =set([1,2,3,5,0,8,7])
+sample_set2=set([1,2,3,9,6,7,8])
+
+#symeteric diffrance set to store diffrance 
+Symeteric_diff_set=sample_set1 ^sample_set2
+
+
+
+
+#printing the symeteric diffrance of set1- set2        
+print(f"union of {sample_set1} and {sample_set2} is : {Symeteric_diff_set}") 
+
