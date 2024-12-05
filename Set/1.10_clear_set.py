16d268fdf2472fcc21c8b33684ed57ef18db3820
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 13:12:45 +0530

    [Aman] Add:Python program to clear a set.

---
+++ Set/1.10_clear_set.py
@@ -0,0 +1,9 @@
+# creating a set 
+sample_set= {1,2,3,4,5}
+
+#iterate over set
+sample_set.clear()
+
+
+#printing the empty set
+print(f"emtpy set after clearing all the items from set : {sample_set}")
