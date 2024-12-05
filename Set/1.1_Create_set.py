eaddf47024571f9a9f19b9d6d259c3464ba9c1e1
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 12:22:19 +0530

    [Aman] Add: Python program to create a set.

---
+++ Set/1.1_Create_set.py
@@ -0,0 +1,15 @@
+# creating a set 
+sample_set= {1,2,3,4,5}
+
+#printing set
+print(sample_set)
+
+#creating a set from list
+sample_list=[1,2,3,4]
+new_set=set(sample_list)
+
+#printing the new set
+print(new_set)
+
+#checking the type of set
+print(type(sample_set))
