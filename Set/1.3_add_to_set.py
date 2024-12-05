4233f9f80fb9823ab21c24087a432202c5f93fe5
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 12:27:42 +0530

    [Aman] Add: Python program to add member(s) in a set.

---
+++ Set/1.3_add_to_set.py
@@ -0,0 +1,10 @@
+# creating a set 
+sample_set= {1,2,3,4,5}
+
+print(f"Before adding an item to set {sample_set}")
+#adding a new item to set 
+sample_set.add(6)
+
+#after adding an item to set 
+print(f"after adding an item to set {sample_set}")
+
