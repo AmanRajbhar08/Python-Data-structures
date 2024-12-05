8a98030ed2c2d43d6ae08d3d11593c614f2fe3b2
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 12:51:42 +0530

    [Aman] Add:Python program to create a union of sets.

---
+++ Set/1.7_set_union.py
@@ -0,0 +1,11 @@
+#sample sets
+sample_set1 =set([1,2,3])
+sample_set2=set([1,2,3,5,6])
+
+#empty set to store interset 
+union_set=sample_set1 | sample_set2
+
+
+
+#printing the union        
+print(f"union of {sample_set1} and {sample_set2} is : {union_set}")        
