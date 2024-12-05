a35265ec16ec80e98804dc8d1c6fc8a50ba6eb9f
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 12:57:44 +0530

    [Aman] Add:Python program to create set difference.

---
+++ Set/1.8_set_diff.py
@@ -0,0 +1,12 @@
+#sample sets
+sample_set1 =set([1,2,3,5,0,8,7])
+sample_set2=set([1,2,3,9,6,7,8])
+
+#diffrance set to store diffrance 
+diff_set=sample_set1.difference(sample_set2)
+
+
+
+
+#printing the diffrance of set1- set2        
+print(f"union of {sample_set1} and {sample_set2} is : {diff_set}") 
