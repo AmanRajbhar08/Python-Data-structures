a1e437c1de925e64bb0b656da30e98a589be809c
AmanRajbhar08 <96625837+AmanRajbhar08@users.noreply.github.com>
2024-12-05 12:30:38 +0530

    [Aman] Add:program to remove item(s) from set

---
+++ Set/1.4_remove_item.py
@@ -0,0 +1,10 @@
+# creating a set 
+sample_set= {1,2,3,4,5}
+
+print(f"Before adding an item to set {sample_set}")
+#removing an item from  set 
+sample_set.remove(2)
+
+#after removing an item from set 
+print(f"after adding an item to set {sample_set}")
+
